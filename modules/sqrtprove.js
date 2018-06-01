1ee0c06610431a8e98bfcbb4ec441e3d09ed9f21
Skybbles // L5474 <skybluehexapon@outlook.com>
2018-06-01 16:54:19 -0400

    oops, forgot to change that color

--- modules/sqrtprove.js
+++ modules/sqrtprove.js
@@ -1,7 +1,7 @@
 const colors = require("colors");
 
 export default function calc(input) {
-    console.log(("Enter the decimal and radical form you want to prove equal.\n").yellow + ("[decimal form], [radical form]\n").yellow);
+    console.log(("Enter the decimal and radical form you want to prove equal.\n").yellow + ("[decimal form], [radical form]\n").gray);
     inp = input.split(" ");
     sqrt = inp[1].split(" ");
     anssqrt = (sqrt[0] * (Math.sqrt(sqrt[1])));
