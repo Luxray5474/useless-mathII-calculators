b35c74350ace6df808a1c9b3f84ccc198bce0bdd
Skybbles // L5474 <skybluehexapon@outlook.com>
2018-06-01 16:53:33 -0400

    Fixed sqrtprove

--- modules/sqrtprove.js
+++ modules/sqrtprove.js
@@ -1,4 +1,5 @@
 const colors = require("colors");
+
 export default function calc(input) {
     console.log(("Enter the decimal and radical form you want to prove equal.\n").yellow + ("[decimal form], [radical form]\n").yellow);
     inp = input.split(" ");
@@ -10,21 +11,5 @@ export default function calc(input) {
     } else {
         console.log(("They do not match.").red);
     }
-    return ans;
-}
-  
-new function sqrtprove() {
-    rl.question(("Enter the decimal and radical form you want to prove equal.\n").yellow + ("[decimal form], [radical form]\n").gray, (answer) => {
-        ans = answer.split(", ");
-        sqrt = ans[1].split(" ");
-        anssqrt = (sqrt[0] * (Math.sqrt(sqrt[1])));
-        console.log(sqrt[0] + "√" + sqrt[1] + " = " + anssqrt);
-        if((anssqrt) == (ans[0])) {
-            console.log(("They match.").green);
-        } else {
-            console.log(("They do not match.").red);
-        }
-        sqrtprove();
-        return;
-    })
+    return;
 }
