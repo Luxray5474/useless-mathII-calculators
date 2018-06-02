f357807e037c4f9d35ba0417349978358bfeb553
Skybbles // L5474 <skybluehexapon@outlook.com>
2018-06-01 21:55:01 -0400

    Made a help module, and fixed some of the other modules

--- modules/anglefind.js
+++ modules/anglefind.js
@@ -1,7 +1,6 @@
 const colors = require('colors');
 
 export default function calc(input) {
-  console.log(('Enter the two known angles. \n').yellow + ('[angle1], [angle2] \n').gray);
   var inp = input.split(", ");
   console.log((180 - (+inp[0] + +inp[1])).green);
 }
---
+++ modules/help.js
@@ -0,0 +1,9 @@
+const colors = require('colors');
+
+console.log(('Command Help:').yellow);
+console.log(('help').bgYellow.black);
+console.log(('  Shows command help and how to use each one.'));
+console.log(('anglef').bgYellow.black);
+console.log(('  Finds an angle given two known angles.'));
+console.log(('  [angle1], [angle2]').gray);
+console.log(('gmean').bgYellow.black);
