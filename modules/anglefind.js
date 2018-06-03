89b169800ae120b529bea62fcbfd2552984103ef
Luxray5474 <skybluehexapon@outlook.com>
2018-06-03 12:21:53 -0400

    I think I'm done :3

--- modules/anglefind.js
+++ modules/anglefind.js
@@ -2,7 +2,12 @@ const colors = require('colors');
 
 module.exports = function calc(input) {
   var inp = (input).split(", ");
-  console.log(colors.green(180 - (+inp[0] + +inp[1])));
+  var ans = 180 - (+inp[0] + +inp[1]);
+  if(ans == 180) {
+    console.log(colors.magenta("180; no arguments passed"));
+  } else { 
+    console.log(colors.green(ans));
+  }
   return;
 }
 
