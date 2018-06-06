18f8f291f2411a6ff6a0d378d7aadd6951dc02ac
Skybbles // L5474 <skybluehexapon@outlook.com>
2018-06-05 20:47:24 -0400

    Fix

--- modules/geomean.js
+++ modules/geomean.js
@@ -1,6 +1,6 @@
 const colors = require('colors');
 
 module.exports = function calc(input) {
-  inp = input.split(", ");
+  inp = input.split(",,");
   console.log(colors.green(Math.sqrt(inp[0] * inp[1])));
 }
